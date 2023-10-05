3ff3a3c94fc05382b707645b0c426e0175fff645
NayeemCseMu <nayeem.cse1@gmail.com>
2023-10-05 16:32:26 +0600

    primary complete

---
+++ script.js
@@ -0,0 +1,31 @@
+// Get references to form elements and response message
+const contactForm = document.getElementById("contact-form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const messageInput = document.getElementById("message");
+const responseMessage = document.getElementById("response-message");
+
+// Event listener for form submission
+contactForm.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    // Perform basic form validation
+    if (!nameInput.value.trim() || !emailInput.value.trim() || !messageInput.value.trim()) {
+        responseMessage.textContent = "Please fill in all fields.";
+        return;
+    }
+
+    // Simulate form submission (you can replace this with actual submission code)
+    // In this example, we're just displaying a success message
+    responseMessage.textContent = "Message sent successfully!";
+    contactForm.reset(); // Clear the form
+
+    // You can add code here to send the form data to a server or API
+});
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
