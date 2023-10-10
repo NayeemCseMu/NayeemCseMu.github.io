9cb2b6a093c8812e2c0d0e18ffd097c40a644039
NayeemCseMu <nayeem.cse1@gmail.com>
2023-10-10 11:49:32 +0600

    theme added | responsive issue fixed

--- script.js
+++ script.js
@@ -30,16 +30,8 @@ function toggleMenu() {
     icon.classList.toggle("open");
   }
 
-  (function () {
-    [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
-    });
+function changeTheme(){
     document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
+        document.body.classList.toggle("dark-mode");
     })
-})();
+}
